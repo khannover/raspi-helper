7f96422f80abca4ee3860389bd618142f1755ba1
Kai <kai@hannover38.de>
2017-07-12 22:06:10 +0200

    Update rebootOnNetworkLoss.sh

--- rebootOnNetworkLoss.sh
+++ rebootOnNetworkLoss.sh
@@ -18,7 +18,19 @@ else
     diff=0
     let diff="($now - $lastdate)/60"
     if [ $diff -gt $TOLERANCE ]; then
-      sudo reboot
+      # let's try to reconnect before reboot
+      echo "No network connection, restarting wlan0"
+      sudo ifdown 'wlan0'
+      sleep 5
+      sudo ifup --force 'wlan0'
+      INETSTATUS=`$CURL -Is http://www.google.de | grep HTTP | cut -d " " -f 2`
+      if [ $? -eq 0 -a $INETSTATUS -eq 200 ]; then
+          # we're lucky. the network is back :-)
+          rm $TMP
+      else
+          # seems that nothing helped so far... let's reboot now
+          sudo reboot
+      fi
     fi
   else
     echo `date +%s` > $TMP
